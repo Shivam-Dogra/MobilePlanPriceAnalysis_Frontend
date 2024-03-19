ec41dca0fffd7a691df2c133fe8649531725aa68
Palak <palak.desai4501@gmail.com>
2024-03-19 16:23:06 -0400

    Feature: Added button and search box in crawl

--- src/Features/Crawler.jsx
+++ src/Features/Crawler.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
+import SearchBar from "../components/SearchBar";
 
 const Crawler = () => {
   return (
-    <div className='text-white flex justify-center items-center text-2xl'>
-      Crawler
-    </div> 
-  )
-}
+    <>
+  <div className="text-white flex justify-center items-center m-8 text-3xl">
+    Web Crawler
+  </div>
+  <div className="flex flex-row justify-center items-center gap-4">
+    <h1></h1>
+    <SearchBar />
+    <Button text="crawl" style={{ width: '120px' }} />
+  </div>
+  <div className="flex justify-center items-center m-4 p-4 border border-gray-300 rounded-lg h-80">
+    Output Text Goes Here
+  </div>
+</>
 
-export default Crawler
+  );
+};
+
+export default Crawler;
