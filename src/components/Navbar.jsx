de4abd035e27e48804e4efe04db4e00d137ab4c4
Palak <palak.desai4501@gmail.com>
2024-03-18 20:06:53 -0400

    Feature: Added dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import { NAV_LINKS } from "../constants";
-import logo from "../assets/mobileLogo.png"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/mobileLogo.png";
+import menuIcon from "../assets/menu.png";
+import { NAV_LINKS, featureLists } from "../constants";
 
 const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <nav className="flex items-center bg-gray-400 h-20 w-screen px-4">
       <div className="flex items-center">
@@ -13,9 +20,8 @@ const Navbar = () => {
         <ul className="flex justify-center items-center gap-4">
           {NAV_LINKS.map((link) => (
             <li key={link.key} className="py-2">
-              {/* Use Link component instead of anchor */}
               <Link
-                to={link.href} // Specify the target path for the link
+                to={link.href}
                 className="font-serif text-2xl text-black cursor-pointer transition-all hover:font-bold hover:bg-black hover:rounded-lg px-4 py-2 hover:text-white"
               >
                 {link.label}
@@ -23,6 +29,30 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+      </div>
+      <div className="flex items-center">
+        <img
+          src={menuIcon}
+          alt="Menu"
+          className="h-6 ml-4 cursor-pointer"
+          onClick={toggleDropdown}
+        />
+        {isDropdownOpen && (
+          <div className="absolute top-20 right-0 bg-gray-400 rounded-lg p-2">
+            <ul>
+              {featureLists.map((link) => (
+                <li key={link.key}>
+                  <Link
+                    to={link.href}
+                    className="text-black hover:text-white"
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
