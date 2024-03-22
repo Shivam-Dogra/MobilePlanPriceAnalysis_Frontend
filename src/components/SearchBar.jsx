7e8c35a841eb9bb0c78ff6b65281c31b6c6cb975
Palak <palak.desai4501@gmail.com>
2024-03-22 02:40:28 -0400

    Feature: Changed margin

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ const SearchBar = ({ placeholder, value, onChange }) => {
   };
 
   return (
-    <div className="relative w-full">
+    <div className="m-4 w-full ">
       <input
         type="text"
         className = "border border-gray-200 rounded-md px-4 py-2 w-full text-base focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
