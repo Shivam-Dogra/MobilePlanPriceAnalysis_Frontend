b22e928cbf8bd1f4f78f4d014f111fc2460165ab
Palak <palak.desai4501@gmail.com>
2024-03-22 01:32:29 -0400

    Feature: Adjust padding

--- src/components/BoxCard.jsx
+++ src/components/BoxCard.jsx
@@ -11,11 +11,11 @@ const BoxCard = ({ plan }) => {
   } = plan;
 
   return (
-    <div className="border-8 border-white p-6 mb-4 rounded-xl bg-gradient-to-br from-gray-800 to-gray-900">
+    <div className="border-8  border-white p-8 mb-4 rounded-xl bg-gradient-to-br from-gray-800 to-gray-900">
   <div className="border-4 border-white rounded-xl bg-gray-300 p-4 size-fit">
     <h1 className="text-black text-2xl font-semibold">{provider}</h1>
   </div>
-  <div className="space-y-6 text-gray-400 text-lg mt-6">
+  <div className="space-y-8 text-gray-400 text-xl mt-6">
     <p className="text-white text-2xl"><strong> {planName} </strong></p>
     <p className="text-white"><strong>Monthly Cost:</strong> {monthlyCost}</p>
     <p className="text-white"><strong>Data Allowance:</strong> {dataAllowance}</p>
