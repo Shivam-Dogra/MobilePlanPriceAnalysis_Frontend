5a712abbba9c3968a8db96864c975ed2dd0a3ec8
Palak <palak.desai4501@gmail.com>
2024-03-22 01:53:42 -0400

    Feature: Removed Card component and Added BoxCard component

--- src/components/BestPlan.jsx
+++ src/components/BestPlan.jsx
@@ -1,42 +1,49 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Import axios for making HTTP requests
+import axios from 'axios';
 import Button from './Button';
+import BoxCard from './BoxCard'; // Import BoxCard component
 
 const BestPlan = () => {
-  const [plans, setPlans] = useState([]); // State to store the fetched plans
-  const [loading, setLoading] = useState(false); // State to track loading status
+  const [plans, setPlans] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCriteria, setSelectedCriteria] = useState(null); // State to track selected criteria
 
   const fetchData = async (criteria) => {
-    setLoading(true); // Set loading to true before making the request
+    setLoading(true);
+    setSelectedCriteria(criteria); // Set selected criteria
     try {
-      const response = await axios.get(`http://localhost:9091/mobile-plans/best-plan/${criteria}`); // Make GET request to backend API
-      setPlans(response.data); // Update plans state with the fetched data
+      const response = await axios.get(`http://localhost:9091/mobile-plans/best-plan/${criteria}`);
+      setPlans(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     } finally {
-      setLoading(false); // Set loading back to false after fetching data
+      setLoading(false);
     }
   };
 
   const handlePriceButtonClick = () => {
-    fetchData('price'); // Fetch plans based on price criteria
+    fetchData('price');
   };
 
   const handleDataButtonClick = () => {
-    fetchData('data'); // Fetch plans based on data criteria
+    fetchData('data');
   };
 
   return (
     <div className="container mx-auto mt-12">
       <h1 className="text-3xl font-bold mb-4 font-serif text-white flex justify-center m-10">Best-Plan</h1>
-      <div className="flex justify-center">
+      <div className="flex justify-center mb-4">
         <Button text="Price" onClick={handlePriceButtonClick} />
         <Button text="Data" onClick={handleDataButtonClick} />
       </div>
       {loading ? (
-        <p  className="text-3xl font-bold text-center mb-8 text-gray-500">Scraping and getting the best plan for you..</p>
+        <p className="text-3xl font-bold text-center mb-8 text-gray-500">Scraping and getting the best plan for you..</p>
       ) : (
-        <Card plans={plans} provider="Provider" />
+        plans.length > 0 && selectedCriteria ? ( // Check if plans exist and a criteria is selected
+          plans.map(plan => <BoxCard key={plan.id} plan={plan} />) // Render BoxCard for each plan
+        ) : (
+          <p className="text-3xl font-bold text-center mb-8 text-gray-500">No plans available.</p>
+        )
       )}
     </div>
   );
