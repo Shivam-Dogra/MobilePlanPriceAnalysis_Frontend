289dac05a465a76e2265777825b0e2f58d73e566
Palak <palak.desai4501@gmail.com>
2024-03-22 03:14:32 -0400

    Feature: Modified margins

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Button = ({ text, onClick, style }) => {
   return (
     <button
-      className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-2"
+      className="bg-gray-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded m-2"
       onClick={onClick}
       style={style}
     >
